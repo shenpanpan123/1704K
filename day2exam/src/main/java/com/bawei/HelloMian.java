2809d28059984fb526b10148510da221e151eb6e
shenpanpan <m18040030328@163.com>
2019-08-07 15:03:28 +0800

    我们再次修改了这个文件

--- day2exam/src/main/java/com/bawei/HelloMian.java
+++ day2exam/src/main/java/com/bawei/HelloMian.java
@@ -4,8 +4,12 @@ public class HelloMian {
 	
 	public static void main(String[] args) {
 		HelloWorld helloWorld = new HelloWorld();
+		
 		helloWorld.sayHello();
 		System.out.println("下午上课好困呀。。。。。。。。");
+		
+		HelloWorld helloWorld2 = new HelloWorld();
+		helloWorld2.sayHello();
 	}
 	
 }
