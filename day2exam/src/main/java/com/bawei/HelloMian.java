9a76c1b2e8bfeae967152ce5d766f5f8d8f5cda5
shenpanpan <m18040030328@163.com>
2019-08-07 15:00:20 +0800

    我们修改了HelloMain这个类，加入了一行输出

--- day2exam/src/main/java/com/bawei/HelloMian.java
+++ day2exam/src/main/java/com/bawei/HelloMian.java
@@ -5,6 +5,7 @@ public class HelloMian {
 	public static void main(String[] args) {
 		HelloWorld helloWorld = new HelloWorld();
 		helloWorld.sayHello();
+		System.out.println("下午上课好困呀。。。。。。。。");
 	}
 	
 }
